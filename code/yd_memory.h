571b0bfa98bbd4849d73e8dd3d3e4c62d8550e0c
Yuval Dolev <yuvaldolev2@gmail.com>
2019-07-10 03:52:11 +0300

    More arena memory management functions implemented

--- code/yd_memory.h
+++ code/yd_memory.h
@@ -46,8 +46,13 @@ struct temporary_memory
     size_t Used;
 }
 
-internal void InitializeArena(memory_arena* Arena, void* Base, size_t Size);
-internal void* PushSize_(memory_arena* Arena, size_t Size, size_t Alignment)
+inline void InitializeArena(memory_arena* Arena, void* Base, size_t Size);
+inline void SubArena(memory_arena* Result, memory_arena* Arena,
+                     size_t Size, size_t Alignment = 16);
+inline void Clear(memory_arena* Arena);
+inline size_t GetAlignmentOffset(memory_arena* Arena, size_t Alignment);
+inline size_t GetEffectiveSizeFor(memory_arena* Arena, size_t SizeInit, size_t Alignment);
+inline void* PushSize_(memory_arena* Arena, size_t SizeInit, size_t Alignment = 4)
 
 #if !defined(PushSize)
 # define PushSize(Arena, Size, ...) PushSize_(Arena, Size, ## __VA_ARGS__)
@@ -64,39 +69,86 @@ internal void* PushSize_(memory_arena* Arena, size_t Size, size_t Alignment)
 inline temporary_memory BeginTemporaryMemory(memory_arena* Arena);
 inline void EndTemporaryMemory(temporary_memory TempMem);
 inline void CheckArena(memory_arena* Arena);
+inline size_t GetArenaSizeRemaining(memory_arena* Arena, size_t Alignemnt = 4);
+inline b32_yd ArenaHasRoomFor(memory_arena* Arena, size_t SizeInit, size_t Alignment = 4);
 
 #if defined(YD_MEMORY_IMPLEMENTATION)
+
+//
+// NOTE(yuval): Arena / SubArena Initialization And Clearing
+//
 
 inline void
 InitializeArena(memory_arena* Arena, void* Base, size_t Size)
 {
-    Arena->Base = (u8*)Base;
+    Arena->Base = (u8_yd*)Base;
     Arena->Size = Size;
     Arena->Used = 0;
     Arena->TempCount = 0;
 }
 
-inline void*
-PushSize_(memory_arena* Arena, size_t Size, size_t Alignment)
+inline void
+SubArena(memory_arena* Result, memory_arena* Arena,
+         size_t Size, size_t Alignment)
 {
-    size_t ResultPointer = (size_t)Arena->Base + Arena->Used;
+    Result->Base = (u8_yd*)PushSize(Arena, Size, Alignment);
+    Result->Size = Size;
+    Result->Used = 0;
+    Result->TempCount = 0;
+}
+
+inline void
+Clear(memory_arena* Arena)
+{
+    InitializeArena(Arena, Arena->Base, Arena->Size);
+}
+
+//
+// NOTE(yuval): Arena Pushing And Alignment
+//
+
+inline size_t
+GetAlignmentOffset(memory_arena* Arena, size_t Alignment)
+{
     size_t AlignmentOffset = 0;
-    
+    size_t TailPointer = (size_t)Arena->Base + Arena->Used;
     size_t AlignmentMask = Alignment - 1;
+    
     if (ResultPointer & AlignmentMask)
     {
-        AlignmentOffset = Alignment - (ResultPointer & AlignmentMask);
+        AlignmentOffset = Alignment - (TailPointer & AlignmentMask);
     }
     
-    Size += AlignmentOffset;
+    return AlignmentOffset;
+}
+
+inline size_t
+GetEffectiveSizeFor(memory_arena* Arena, size_t SizeInit, size_t Alignment)
+{
+    size_t AlignmentOffset = GetAlignmentOffset(Arena, Alignment);
+    size_t Size = SizeInit + AlignmentOffset;
+    AssertYD(Size >= SizeInit);
     
-    Assert((Arena->Used + Size) <= Arena->Size);
+    return Size;
+}
+
+inline void*
+PushSize_(memory_arena* Arena, size_t SizeInit, size_t Alignment)
+{
+    size_t Size = GetEffectiveSizeFor(Arena, SizeInit, Alignment);
+    
+    AssertYD((Arena->Used + Size) <= Arena->Size);
+    
+    size_t AlignmentOffset = GetAlignmentOffset(Arena, Alignment);
+    void* Result = Arena->Base + Arena->Used + AlignmentOffset;
     Arena->Used += Size;
-    
-    void* Result = (void*)(ResultPointer + AlignmentOffset);
     
     return Result;
 }
+
+//
+// NOTE(yuval): Temporary Arena Memory
+//
 
 inline temporary_memory
 BeginTemporaryMemory(memory_arena* Arena)
@@ -127,6 +179,26 @@ inline void
 CheckArena(memory_arena* Arena)
 {
     AssertYD(Arena->TempCount == 0);
+}
+
+//
+// NOTE(yuval): Arena Size
+//
+
+inline void
+GetArenaSizeRemaining(memory_arena* Arena, size_t Alignment)
+{
+    size_t Result = Arena->Size - (Arena->Used + GetAlignmentOffset(Arena, Alignment));
+    return Result;
+}
+
+inline b32_yd
+ArenaHasRoomFor(memory_arena* Arena, size_t SizeInit, size_t Alignment)
+{
+    size_t Size = GetEffectiveSizeFor(Arena, SizeInit, Alignment);
+    b32_yd Result = ((Arena->Used + Size) <= Arena->Size);
+    
+    return Result;
 }
 
 #endif
