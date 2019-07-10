92a12750fc2b9c6b820387abeebeab4ca9cc5af6
Yuval Dolev <yuvaldolev2@gmail.com>
2019-07-10 13:19:12 +0300

    Added arena push parameters

--- code/yd_memory.h
+++ code/yd_memory.h
@@ -44,15 +44,45 @@ struct temporary_memory
 {
     memory_arena* Arena;
     size_t Used;
-}
+};
 
+enum arena_push_flag
+{
+    ArenaFlag_ClearToZero = 0x1
+};
+
+struct arena_push_params
+{
+    u32_yd Flags;
+    u32_yd Alignment;
+};
+
+internal void* Copy(void* DestInit, const void* SourceInit, size_t Size);
+
+#if !defined(CopyArray)
+# define CopyArray(Dest, Source, Count) Copy((Dest), (Source), (Count) * sizeof(*(Source)))
+#endif
+
+internal void* ZeroSize(void* Ptr, size_t Size);
+
+#if !defined(ZeroStruct)
+# define ZeroStruct(Instance) ZeroSize(&(Instance), sizeof(Instance))
+#endif
+
+#if !defined(ZeroArray)
+# define ZeroArray(Pointer, Count) ZeroSize(Pointer, (Count) * sizeof(*(Pointer)))
+#endif
+
+inline arena_push_params DefaultArenaParams();
 inline void InitializeArena(memory_arena* Arena, void* Base, size_t Size);
 inline void SubArena(memory_arena* Result, memory_arena* Arena,
-                     size_t Size, size_t Alignment = 16);
+                     size_t Size, arena_push_params Params = DefaultArenaParams());
 inline void Clear(memory_arena* Arena);
 inline size_t GetAlignmentOffset(memory_arena* Arena, size_t Alignment);
-inline size_t GetEffectiveSizeFor(memory_arena* Arena, size_t SizeInit, size_t Alignment);
-inline void* PushSize_(memory_arena* Arena, size_t SizeInit, size_t Alignment = 4)
+inline size_t GetEffectiveSizeFor(memory_arena* Arena, size_t SizeInit,
+                                  arena_push_params Params = DefaultArenaParams());
+inline void* PushSize_(memory_arena* Arena, size_t SizeInit,
+                       arena_push_params Params = DefaultArenaParams())
 
 #if !defined(PushSize)
 # define PushSize(Arena, Size, ...) PushSize_(Arena, Size, ## __VA_ARGS__)
@@ -63,16 +93,68 @@ inline void* PushSize_(memory_arena* Arena, size_t SizeInit, size_t Alignment = 
 #endif
 
 #if !defined (PushArray)
-# define PushArray(Arena, Type, Count, ...) (Type*)PushSize_(Arena, (Count) * sizeof(Type), ## __VA_ARGS__)
+# define PushArray(Arena, Type, Count, ...) (Type*)PushSize_(Arena, (Count) * sizeof(Type), \
+## __VA_ARGS__)
 #endif
 
 inline temporary_memory BeginTemporaryMemory(memory_arena* Arena);
 inline void EndTemporaryMemory(temporary_memory TempMem);
 inline void CheckArena(memory_arena* Arena);
-inline size_t GetArenaSizeRemaining(memory_arena* Arena, size_t Alignemnt = 4);
-inline b32_yd ArenaHasRoomFor(memory_arena* Arena, size_t SizeInit, size_t Alignment = 4);
+inline size_t GetArenaSizeRemaining(memory_arena* Arena,
+                                    arena_push_params Params = DefaultArenaParams());
+inline b32_yd ArenaHasRoomFor(memory_arena* Arena, size_t SizeInit,
+                              arena_push_params Params = DefaultArenaParams());
 
 #if defined(YD_MEMORY_IMPLEMENTATION)
+
+//
+// NOTE(yuval): Memory Helper Functions
+//
+
+internal void*
+Copy(void* DestInit, const void* SourceInit, size_t Size)
+{
+    void* Result = 0;
+    
+    if (DestInit && SourceInit)
+    {
+        const u8_yd* Source = (const u8_yd*)SourceInit;
+        u8_yd* Dest = (u8_yd*)DestInit;
+        
+        while (Size--)
+        {
+            *Dest++ = *Source++;
+        }
+        
+        Result = DestInit;
+    }
+    
+    return Result;
+}
+
+internal void*
+ZeroSize(void* Ptr, size_t Size)
+{
+    u8_yd* Byte = (u8_yd*)Ptr;
+    
+    while (Size--)
+    {
+        *Byte++ = 0;
+    }
+}
+
+//
+// NOTE(yuval): Arena Push Parameters
+//
+
+inline arena_push_params
+DefaultArenaParams()
+{
+    arena_push_params Params;
+    Params.Flags = ArenaFlag_ClearToZero;
+    Params.Alignment = 4;
+}
+
 
 //
 // NOTE(yuval): Arena / SubArena Initialization And Clearing
@@ -89,9 +171,9 @@ InitializeArena(memory_arena* Arena, void* Base, size_t Size)
 
 inline void
 SubArena(memory_arena* Result, memory_arena* Arena,
-         size_t Size, size_t Alignment)
+         size_t Size, arena_push_params Params)
 {
-    Result->Base = (u8_yd*)PushSize(Arena, Size, Alignment);
+    Result->Base = (u8_yd*)PushSize(Arena, Size, Params);
     Result->Size = Size;
     Result->Used = 0;
     Result->TempCount = 0;
@@ -123,9 +205,9 @@ GetAlignmentOffset(memory_arena* Arena, size_t Alignment)
 }
 
 inline size_t
-GetEffectiveSizeFor(memory_arena* Arena, size_t SizeInit, size_t Alignment)
+GetEffectiveSizeFor(memory_arena* Arena, size_t SizeInit, arena_push_params Params)
 {
-    size_t AlignmentOffset = GetAlignmentOffset(Arena, Alignment);
+    size_t AlignmentOffset = GetAlignmentOffset(Arena, Params.Alignment);
     size_t Size = SizeInit + AlignmentOffset;
     AssertYD(Size >= SizeInit);
     
@@ -133,15 +215,20 @@ GetEffectiveSizeFor(memory_arena* Arena, size_t SizeInit, size_t Alignment)
 }
 
 inline void*
-PushSize_(memory_arena* Arena, size_t SizeInit, size_t Alignment)
+PushSize_(memory_arena* Arena, size_t SizeInit, arena_push_params Params)
 {
-    size_t Size = GetEffectiveSizeFor(Arena, SizeInit, Alignment);
+    size_t Size = GetEffectiveSizeFor(Arena, SizeInit, Params);
     
     AssertYD((Arena->Used + Size) <= Arena->Size);
     
-    size_t AlignmentOffset = GetAlignmentOffset(Arena, Alignment);
+    size_t AlignmentOffset = GetAlignmentOffset(Arena, Params.Alignment);
     void* Result = Arena->Base + Arena->Used + AlignmentOffset;
     Arena->Used += Size;
+    
+    if (Params.Flags & ArenaFlag_ClearToZero)
+    {
+        ZeroSize(Result, SizeInit);
+    }
     
     return Result;
 }
@@ -186,16 +273,16 @@ CheckArena(memory_arena* Arena)
 //
 
 inline void
-GetArenaSizeRemaining(memory_arena* Arena, size_t Alignment)
+GetArenaSizeRemaining(memory_arena* Arena, arena_push_params Params)
 {
-    size_t Result = Arena->Size - (Arena->Used + GetAlignmentOffset(Arena, Alignment));
+    size_t Result = Arena->Size - (Arena->Used + GetAlignmentOffset(Arena, Params.Alignment));
     return Result;
 }
 
 inline b32_yd
-ArenaHasRoomFor(memory_arena* Arena, size_t SizeInit, size_t Alignment)
+ArenaHasRoomFor(memory_arena* Arena, size_t SizeInit, arena_push_params Params)
 {
-    size_t Size = GetEffectiveSizeFor(Arena, SizeInit, Alignment);
+    size_t Size = GetEffectiveSizeFor(Arena, SizeInit, Params);
     b32_yd Result = ((Arena->Used + Size) <= Arena->Size);
     
     return Result;
