8b81929c658cee5cda300603bf0be71abb33100d
Yuval Dolev <yuvaldolev2@gmail.com>
2019-07-24 16:08:56 +0300

    Updated the travis build file.

--- code/build_travis.sh
+++ code/build_travis.sh
@@ -2,7 +2,7 @@
 
 CommonFlags="-g -Weverything -Wall -Werror -fdiagnostics-absolute-paths -std=c++11 -fno-rtti -fno-exceptions"
 CommonFlags+=" -Wno-unsequenced -Wno-comment -Wno-unused-variable -Wno-unused-function -Wno-unused-result -Wno-switch -Wno-old-style-cast -Wno-zero-as-null-pointer-constant -Wno-string-conversion  -Wno-newline-eof -Wno-c++98-compat-pedantic -Wno-gnu-anonymous-struct -Wno-nested-anon-types -Wno-unused-parameter -Wno-padded -Wno-missing-prototypes -Wno-cast-align -Wno-sign-conversion -Wno-switch-enum -Wno-double-promotion -Wno-gnu-zero-variadic-macro-arguments -Wno-missing-noreturn -Wno-class-varargs -Wno-deprecated-declarations -Wno-documentation-unknown-command -Wno-weak-vtables -Wno-cast-qual"
-CommonFlags+=" -DBUILD_INTERNAL=1 -DBUILD_SLOW=1"
+CommonFlags+=" -DBUILD_INTERNAL=1 -DBUILD_SLOW=1 -I../run_tree/code"
 
 # NOTE(yuval): Setup compiler
 if [ -n "$(command -v clang++)" ]
@@ -17,12 +17,14 @@ fi
 # TODO(yuval): Only darwin is supported for now
 PathFlags="-Wl,-rpath,@loader_path"
 
-mkdir -p "../build"
-pushd "../build" > /dev/null
+mkdir -p "../.build"
+pushd "../.build" > /dev/null
 
 echo "Compiling Using: $CXX"
 
 # Mac Build
 CommonFlags+=" -DBUILD_MAC=1 -DBUILD_TRAVIS=1"
 MacFlags="-framework Cocoa -framework IOKit -framework Security" #-framework OpenGL -framework AudioToolbox"
-$CXX $CommonFlags ../code/mac_build.mm -o mac_build -ldl $MacFlags $PathFlags
+$CXX $CommonFlags ../code/mac_build.mm -o ../run_tree/mac_build -ldl $MacFlags $PathFlags
+
+popd > /dev/null
